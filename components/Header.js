d43e2d7a3b91c1fc699ee6179fb9ca5ab7e13606
davidgeorgebell <bell.dave93@gmail.com>
2020-11-10 14:58:52 +0800

    bug fixes, read me

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header className="py-6 flex justify-between px-4">
+    <header className="py-6 flex justify-between">
       <Link href="/">
         <a>
           <h1 className="text-2xl md:text-4xl font-black text-orange-400 tracking-wider">
